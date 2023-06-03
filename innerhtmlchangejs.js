377317921bbddff9257218058d4726a0953b5505
deepika yadav <84773214+Deepika16008@users.noreply.github.com>
2023-06-03 01:45:19 +0000

    Add files via upload

---
+++ innerhtmlchangejs.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "avaScript Can Change HTML and this a paragrap which im writing noe just to check is it dong well work aur not  to check its validation im ui=ing tis dom manupliation .so that i can check its text is changing aur nt accordingly. when we click on the tbutton then its content means its text iis changinfg aur not we can check that . each paragraph has a different content . when we click on the button then its text is cahnging or not .we can check that so  this paragraph is in the first button so i can copy this pararapgh to make it easy to apply in diffrernt sextion so im doing this now. ohk so dont disturb me.";
+}
+
+
+
+function myFunction1() {
+    document.getElementById("demo").innerHTML = "hello avaScript Can Change  and  a paragrap which im writing noe just to check is it dong well work aur not  to check its validation im ui=ing tis dom manupliation .so that i can check its text is changing aur nt accordingly. when we click on the tbutton then its content means its text iis changinfg aur not we can check that . each paragraph has a different content . when we click on the button then its text is cahnging or not .we can check that so  this paragraph is in the first button so i can copy this pararapgh to make it easy to apply in diffrernt sextion so im doing this now. ohk so dont disturb me.";
+}
+function myFunction2() {
+    document.getElementById("demo").innerHTML = "hi java avaScript Can Change  and thi a paragrap which im writing noe just to check is it dong well work aur not  to check its validation im ui=ing tis dom manupliation .so that i can check its text is changing aur nt accordingly. when we click on the tbutton then its content means its text iis changinfg aur not we can check that . each paragraph has a different content . when we click on the button then its text is cahnging or not .we can check that so  thi paragraph is i the first button so i can copy thi pararapgh to make it easy to apply i diffrernt sextion so im doing  now. ohk so dont disturb me.";
+}
